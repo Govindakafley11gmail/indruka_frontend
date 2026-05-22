11a2bc7ca16019f710ac73212a6516f4a6baec1b
Govindakafley11gmail <Govindakafley11gmail@gmail.com>
2026-05-22 11:24:43 +0600

    removal of privacy

--- custom-components/home-tabs/tour_packages_tabs.tsx
+++ custom-components/home-tabs/tour_packages_tabs.tsx
@@ -8,24 +8,25 @@ import Image from "next/image";
 export default function TourCategories() {
   return (
     <section className="relative min-h-screen ">
-      {/* HEADER */}
-      <div className="text-center mb-14 max-w-2xl mx-auto">
-        <span className="inline-block text-xs font-semibold tracking-widest uppercase  mb-4 px-4 py-1.5 bg-blue-100 rounded-full shadow-sm text-black">
-          Travel For You
-        </span>
+      <div className="text-center mb-10 sm:mb-12 md:mb-14 max-w-2xl mx-auto px-4 sm:px-6">
+  
+  <span className="inline-block text-[10px] sm:text-xs font-semibold tracking-widest uppercase mb-3 sm:mb-4 px-3 sm:px-4 py-1 sm:py-1.5 bg-blue-100 rounded-full shadow-sm text-black">
+    Travel For You
+  </span>
 
-        <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight mb-4">
-          Explore Your{" "}
-          <span className="bg-gradient-to-r from-blue-500 to-blue-400 bg-clip-text text-transparent">
-            Dream Destinations
-          </span>
-        </h2>
+  <h2 className="text-2xl sm:text-3xl md:text-5xl font-extrabold text-gray-900 leading-snug sm:leading-tight mb-3 sm:mb-4">
+    Explore Your{" "}
+    <span className="bg-gradient-to-r from-blue-500 to-blue-400 bg-clip-text text-transparent">
+      Dream Destinations
+    </span>
+  </h2>
 
-        <p className="text-gray-500 text-base md:text-lg leading-relaxed">
-          Craft a personalized getaway tailored to your preferences and desires.
-          Every journey begins with a single step.
-        </p>
-      </div>
+  <p className="text-sm sm:text-base md:text-lg  text-gray-500 leading-relaxed px-1 sm:px-2">
+    Craft a personalized getaway tailored to your preferences <br/>and desires.  
+    Every journey begins with a single step.
+  </p>
+
+</div>
 
       {/* TABS */}
       <div className="min-w-7xl mx-auto ">
