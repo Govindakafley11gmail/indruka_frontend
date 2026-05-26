921c96ef7ad7f828b346c1357e4933c0b9aa54c6
Govindakafley11gmail <Govindakafley11gmail@gmail.com>
2026-05-26 11:35:24 +0600

    webp files

--- custom-components/cards/index.tsx
+++ custom-components/cards/index.tsx
@@ -96,7 +96,7 @@ export default function TourCard({
       : (price ?? 0);
   return (
     <>
-      <Card className="overflow-hidden h-full rounded-2xl  shadow-xl border p-0 w-[330px] ">
+      <Card className="overflow-hidden h-full rounded-2xl  shadow-xl border p-0  ">
         {/* Image */}
         <div className="relative h-50 overflow-hidden">
           <Image
